49b0e9c5f61f5485858a47289d081d7d986b65aa
marc palet <marc.palet@gmail.com>
2025-05-31 08:48:45 +0200

    update users

--- configuration.nix
+++ configuration.nix
@@ -129,25 +129,16 @@
   # put your own configuration here, for example ssh keys:
   users.defaultUserShell = pkgs.zsh;
   users.mutableUsers = true;
-  users.groups = {
-    nixos = {
-      gid = 1000;
-      name = "marc";
-    };
-  };
   users.users = {
     marc = {
+      isNormalUser = true;
       uid = 1000;
-      home = "/home/marc";
-      name = "marc";
-      group = "marc";
-      shell = pkgs.zsh;
+      home = "/home/marc"
       extraGroups = [ "wheel" "docker" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClPIanBAeoqiz3vFLtQdS5lMHeaqtUD8aHPZ0z8JtkYVTiWxv4qHaD9RkPnxgnihAB2oZ+mKEQcHeKi55Qt5fWXr4ytBwSIIBfaX3r4IfuQkAkFHWW0izKz9K6k7xHVFLdjxaCI1PKo7ApH4cpCRHMrANHDdfr5zL1vwRVv3S/uWm5dXVvUKh/Uu2fMi/wYCGXAzOmpQIRlT2Uid1+r8u0Q08H09j/pQn+7OTAHgjaQmf7eZNN7wHfPz4kAOqQHZGnLJ2tKWTBMn9YvgzxJcjqkRwgBNVthMEzfX5M1ymPHZjPxpsD4CiY89mdnBcQ0vVI7CqURysiFL0100n3VS9x marc@localhost"
+      ];
     };
   };
-  users.users.root.openssh.authorizedKeys.keys = [
-    # This is my public key
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClPIanBAeoqiz3vFLtQdS5lMHeaqtUD8aHPZ0z8JtkYVTiWxv4qHaD9RkPnxgnihAB2oZ+mKEQcHeKi55Qt5fWXr4ytBwSIIBfaX3r4IfuQkAkFHWW0izKz9K6k7xHVFLdjxaCI1PKo7ApH4cpCRHMrANHDdfr5zL1vwRVv3S/uWm5dXVvUKh/Uu2fMi/wYCGXAzOmpQIRlT2Uid1+r8u0Q08H09j/pQn+7OTAHgjaQmf7eZNN7wHfPz4kAOqQHZGnLJ2tKWTBMn9YvgzxJcjqkRwgBNVthMEzfX5M1ymPHZjPxpsD4CiY89mdnBcQ0vVI7CqURysiFL0100n3VS9x marc@localhost"
-  ];
   system.stateVersion = "23.05";
 }
